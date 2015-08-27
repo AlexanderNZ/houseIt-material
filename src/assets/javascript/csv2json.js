8531bb2456396ed806b80a544301ef0d27285891
Alex Corkin <alex.corkin@sparkventures.co.nz>
2015-08-28 11:09:10 +1200

    check tradme api keyis correct, add &26 to trademe query string

--- src/assets/javascript/csv2json.js
+++ src/assets/javascript/csv2json.js
@@ -1,7 +1,16 @@
-//parse the data file
-var csvfile = "assets/Auckland.csv";
 
-$.get(csvfile, function (data) {
-  var csvdata = Papa.parse(data);
-  console.log(csvdata);
-});
+//Suburb data is calculated on residential property sales from the previous 3 months
+function createSuburbObject(data) {
+
+  //TODO: Build a JSON model of .csv file and store details into JSON objects
+  var i;
+  while(i < data.length ){
+      this.numberOfSales = num_sales;
+      this.median_price = median_price;
+      this.cv_difference = cv_difference;
+      this.trademe_suburb_id = trademe_suburb_id;
+
+    }
+
+  }
+}
