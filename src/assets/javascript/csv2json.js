e221e42e8018ed82843f819011b906aa6205ede6
kanocarra <emily.melhuish@gmail.com>
2015-08-28 14:27:04 +1200

    Suburb names appended one at a time rather than as one big string

--- src/assets/javascript/csv2json.js
+++ src/assets/javascript/csv2json.js
@@ -1,15 +1,16 @@
 //Generates a list of suburbs from a given .csv file
 //TODO: Sort this by region so that there's not lots of suburbs on screen and give each suburb an individual ID (maybe from TradeMe?)
 function getSuburbs(){
-  //parse the data file
+  //Parse the data file using Papa Parse
   var csvfile = "assets/Auckland.csv";
   $.get(csvfile, function (data) {
     var csvdata = Papa.parse(data);
-    var listedSuburbs = "";
+    var suburb;
     for (var i = 1; i < csvdata.data.length; i++) {
-      listedSuburbs += "<li><a href=\"#\">"+ csvdata.data[i][0] +"</a></li>";
+      //Grab the suburb name and add that to the suburb selector menu
+      suburb = csvdata.data[i][0];
+      $("#suburb-selector").append( "<li><a href=\"#\">"+ suburb +"</a></li>");
     }
-    $("#suburb-selector").append(listedSuburbs);
   });
 }
 
